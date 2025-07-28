e6a88a5299e786079f192c7c9ac7e581107cb864
이승효 <lees513307@gmail.com>
2025-07-28 16:52:03 +0900

    Day3

---
+++ summer vacation C class/기본구조.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello World!\n");
+	printf("떡볶이 튀김 순대\n");
+	printf("강아지\t");
+	printf("고양이\t");
+	printf("토끼\n");
+	printf("dogdogdog\tcat\trabbit\t");
+	printf("강아지는\"멍멍\"고양이는\"야옹\"토끼는 몰라");
+	return 0;
+}
